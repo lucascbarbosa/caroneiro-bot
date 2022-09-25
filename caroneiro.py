feeda0dc6a0761a3366e7641891a3c4509215f8f
Lucas <40960766+lucascbarbosa@users.noreply.github.com>
2022-09-25 16:21:19 -0300

    Update caroneiro.py

--- caroneiro.py
+++ caroneiro.py
@@ -25,47 +25,58 @@ class Caroneiro(object):
         update.message.reply_text("Your Message")
     
     def add_carona(self, update, context):
-        args = update.message['text'].split()
+        args = update.message.text.split()
         if len(args) > 1:
             horario = self.convert_horario(args[1])
             vagas = args[2]
             local = args[3]
             user = update.message.from_user
-            username = user['username']
+            username = user.username
             # msg_carona = f'@{username} - {horario} de {local} ({vagas} vagas)'
             # update.message.reply_text(msg_carona)
         
-        if args[0] == '/ida':
-            if username in self.caronas_ida[:,0]:
-                idx = np.where(self.caronas_ida[:,0]==username)[0]
-                self.caronas_ida[idx,1] = horario
-                self.caronas_ida[idx,2] = vagas
-                self.caronas_ida[idx,3] = local
-            else:
-                self.caronas_ida = np.append(self.caronas_ida,[[username, horario, vagas,local]],axis=0)
-        
-        if args[0] == '/volta':
-            if username in self.caronas_volta[:,0]:
-                idx = np.where(self.caronas_volta[:,0]==username)[0]
-                self.caronas_volta[idx,1] = horario
-                self.caronas_volta[idx,2] = vagas
-                self.caronas_volta[idx,3] = local
-            else:
-                self.caronas_volta = np.append(self.caronas_volta,[[username, horario, vagas,local]],axis=0)
+            if args[0] == '/ida':
+                if username in self.caronas_ida[:,0]:
+                    idx = np.where(self.caronas_ida[:,0]==username)[0]
+                    self.caronas_ida[idx,1] = horario
+                    self.caronas_ida[idx,2] = vagas
+                    self.caronas_ida[idx,3] = local
+                else:
+                    self.caronas_ida = np.append(self.caronas_ida,[[username, horario, vagas,local]],axis=0)
+            
+            if args[0] == '/volta':
+                if username in self.caronas_volta[:,0]:
+                    idx = np.where(self.caronas_volta[:,0]==username)[0]
+                    self.caronas_volta[idx,1] = horario
+                    self.caronas_volta[idx,2] = vagas
+                    self.caronas_volta[idx,3] = local
+                else:
+                    self.caronas_volta = np.append(self.caronas_volta,[[username, horario, vagas,local]],axis=0)
 
-    def get_horario(self, update, context):
-        args = context.args
-        print(update.message)
-        horario_ida = self.convert_horario(args[0])
-        horario_volta = self.convert_horario(args[1])
-        user = update.message.from_user
-        username = user['username']
-        if username in self.horarios[:,0]:
-            idx = np.where(self.horarios[:,0]==username)[0]
-            self.horarios[idx,1] = horario_ida
-            self.horarios[idx,2] = horario_volta
+    def get_set_horario(self, update, context):
+        channel = update.message.chat.type
+        if channel == 'private':
+            args = context.args
+            if len(args) > 0:
+                horario_ida = self.convert_horario(args[0])
+                horario_volta = self.convert_horario(args[1])
+                user = update.message.from_user
+                username = user.username
+                if username in self.horarios[:,0]:
+                    idx = np.where(self.horarios[:,0]==username)[0]
+                    self.horarios[idx,1] = horario_ida
+                    self.horarios[idx,2] = horario_volta
+                else:
+                    self.horarios = np.append(self.horarios, [[username, horario_ida, horario_volta]],axis=0)
+                msg = f"Horários de @{username}:\nIda: {horario_ida}\nVolta: {horario_volta}"
+            else:
+                user = update.message.from_user
+                username = user.username
+                idx = np.where(self.horarios[:,0]==username)[0][0]
+                msg = f"Horários de @{username}:\nIda: {self.horarios[idx,1]}\nVolta: {self.horarios[idx,2]}"
+            update.message.reply_text(msg)
         else:
-            self.horarios = np.append(self.horarios, [[username, horario_ida, horario_volta]],axis=0)
+            pass
 
 def main():
     
@@ -94,9 +105,9 @@ def main():
         )
 
     dispatcher.add_handler(
-            CommandHandler('avisa', caroneiro.get_horario)
+            CommandHandler('hora', caroneiro.get_set_horario)
         )
-
+    
 
     # Start the Bot
     updater.start_polling()
