901a150e9a893b8fd81814ded1990508ec7ecea1
Lucas <40960766+lucascbarbosa@users.noreply.github.com>
2022-11-04 16:24:05 -0300

    Update caroneiro.py

--- caroneiro.py
+++ caroneiro.py
@@ -26,15 +26,16 @@ class Caroneiro(object):
         channel = update.message.chat.type
         if channel == 'private':
             msg = """O caroneiro te avisa das caronas que te interessam. LEMBRANDO QUE ESTE BOT SÓ FUNCIONA NO PRIVADO. Os comandos são:
-            \t/hora [horario_ida] [horario_volta] -> Para informar ou atualizar os horários de ida e volta. Caso queira somente um dos horários, mantenha o outro como 0.
-            \tEx: /hora 8 15\n
-            \tInforma que os horários de ida e volta desejados são 8:00 e 10:00 respectivamente.\n
-            \t/remover -> remove os horários informados."""
-            
+            \t/hora [ida/volta] [horario de ida/volta inicial] [horario de ida/volta final] -> Informa ou atualiza a janela de horários de ida ou volta. Caso queira somente um horário, digite o mesmo horário duas vezes. Para visualizar os horários, digite somente /hora.
+            \tEx: /hora ida 8 8 para IDA às 8:00
+            \tEx: /hora volta 15 16 para VOLTA entre 15:00 e 16:00
+            \t/remover [ida/volta]-> remove os horários de ida ou volta informados.
+            \tEx: /remover ida para remover horários de IDA.
+            \t/avisa -> HABILITA o bot para avisar de caronas.
+            \t/silencia -> SILENCIA o bot."""
             update.message.reply_text(msg)
         
     def add_carona(self, update, context):
-        print('foo')
         args = update.message.text.split()
         if len(args) > 1:
             horario = self.convert_horario(args[1])
@@ -54,9 +55,9 @@ class Caroneiro(object):
                 else:
                     self.caronas_ida = np.append(self.caronas_ida,[[username, horario, vagas,local]],axis=0)
                 # check if this ride is desired by anyone in the database
-                for idx in np.where(self.horarios[:,1] == horario)[0]:
+                for idx in np.where((self.horarios[:,1]<=horario)&(self.horarios[:,2]>=horario))[0]:
                     chat_id = self.horarios[idx,0]
-                    msg = f"Aviso de carona de ida de @{username} às {horario} com {vagas} vagas saindo de {local}"
+                    msg = f"IDA: Carona de @{username} às {horario} com {vagas} vagas saindo de {local}"
                     context.bot.send_message(chat_id, msg)
 
             if args[0] == '/volta':
@@ -68,53 +69,86 @@ class Caroneiro(object):
                 else:
                     self.caronas_volta = np.append(self.caronas_volta,[[username, horario, vagas,local]],axis=0)
                 # check if this ride is desired by anyone in the database
-                for idx in np.where(self.horarios[:,2] == horario)[0]:
+                for idx in np.where((self.horarios[:,3]<=horario)&(self.horarios[:,4]>=horario))[0]:
                     chat_id = self.horarios[idx,0]
-                    msg = f"Aviso de carona de volta de @{username} às {horario} com {vagas} vagas saindo de {local}"
+                    msg = f"VOLTA: Carona de @{username} às {horario} com {vagas} vagas saindo de {local}"
                     context.bot.send_message(chat_id, msg)
 
     def get_set_horario(self, update, context):
         channel = update.message.chat.type
         if channel == 'private':
             args = context.args
-            if len(args) > 0:
-                horario_ida_inicio = self.convert_horario(args[0])
-                horario_ida_fim = self.convert_horario(args[1])
-                horario_volta_inicio = self.convert_horario(args[2])
-                horario_volta_fim = self.convert_horario(args[3])
-
+            if len(args) > 0: # set hora
+                trajeto = args[0]
                 user = update.message.from_user
                 username = user.username
                 chat_id = str(update.message.chat.id)
                 if chat_id in self.horarios[:,0]:
                     idx = np.where(self.horarios[:,0]==chat_id)[0]
-                    self.horarios[idx,1] = horario_ida_inicio
-                    self.horarios[idx,2] = horario_ida_fim
-                    self.horarios[idx,3] = horario_volta_inicio
-                    self.horarios[idx,4] = horario_volta_fim
-                else:
-                    self.horarios = np.append(self.horarios, [[int(chat_id), horario_ida_inicio,horario_ida_fim, horario_volta_inicio, horario_volta_fim]],axis=0)
+                    if trajeto == "ida":
+                        horario_ida_inicio = self.convert_horario(args[1])
+                        horario_ida_fim = self.convert_horario(args[2])
+                        self.horarios[idx,1] = horario_ida_inicio
+                        self.horarios[idx,2] = horario_ida_fim
                 
-                msg = f"Horários de @{username}:\n"
+                    elif trajeto == "volta":
+                        horario_volta_inicio = self.convert_horario(args[1])
+                        horario_volta_fim = self.convert_horario(args[2])
+                        self.horarios[idx,3] = horario_volta_inicio
+                        self.horarios[idx,4] = horario_volta_fim
 
-                if horario_ida_inicio != "0:00" and horario_ida_fim != "0:00":
+                else:
+                    if trajeto == "ida":
+                        horario_ida_inicio = self.convert_horario(args[1])
+                        horario_ida_fim = self.convert_horario(args[2])
+                        self.horarios = np.append(self.horarios, [[int(chat_id), horario_ida_inicio,horario_ida_fim, "0:00", "0:00"]],axis=0)
+                    if trajeto == "volta":
+                        horario_volta_inicio = self.convert_horario(args[1])
+                        horario_volta_fim = self.convert_horario(args[2])
+                        self.horarios = np.append(self.horarios, [[int(chat_id), "0:00", "0:00", horario_volta_inicio, horario_volta_fim]],axis=0)
+
+                if trajeto == "ida":
+                    msg = f"Horários de @{username} para IDA: "
                     if horario_ida_inicio == horario_ida_fim:
-                        msg += f"Ida: {horario_ida_inicio}\n"
+                        msg += f"{horario_ida_inicio}"
                     else:
-                        msg += f"Ida: {horario_ida_inicio}-{horario_ida_fim}\n"
-                if horario_volta_inicio != "0:00" and horario_volta_fim != "0:00":
-                    if horario_volta_inicio == horario_volta_fim:
-                        msg += f"Volta: {horario_volta_inicio}\n"
-                    else:
-                        msg += f"Volta: {horario_volta_inicio}-{horario_volta_fim}\n"
+                        msg += f"{horario_ida_inicio}-{horario_ida_fim}"
                 
-            else:
+                elif trajeto == "volta":
+                    msg = f"Horários de @{username} para VOLTA: "
+                    if horario_volta_inicio == horario_volta_fim:
+                        msg += f"{horario_volta_inicio}"
+                    else:
+                        msg += f"{horario_volta_inicio}-{horario_volta_fim}"
+
+                else:
+                    msg = "Erro. Informe o trajeto correto."
+            
+            else: # get hora
                 user = update.message.from_user
                 username = user.username
                 chat_id = str(update.message.chat.id)
                 try:
                     idx = np.where(self.horarios[:,0]==chat_id)[0][0]
-                    msg = f"Horários de @{username}:\nIda: {self.horarios[idx,1]}\nVolta: {self.horarios[idx,2]}"
+                    horario_ida_inicio = self.horarios[idx,1]
+                    horario_ida_fim = self.horarios[idx,2]
+                    horario_volta_inicio = self.horarios[idx,3]
+                    horario_volta_fim = self.horarios[idx,4]
+                    if horario_ida_inicio != "0:00" and horario_ida_fim != "0:00" and horario_volta_inicio != "0:00" and horario_volta_fim != "0:00":  
+                        msg = f"Horários de @{username}:\n"
+                        if horario_ida_inicio != "0:00" and horario_ida_fim != "0:00":
+                            if horario_ida_inicio == horario_ida_fim:
+                                msg  += f"IDA: {horario_ida_inicio}\n"
+                            else:
+                                msg  += f"IDA: {horario_ida_inicio}-{horario_ida_fim}\n"
+                        
+                        if horario_volta_inicio != "0:00" and horario_volta_fim != "0:00":
+                            if horario_volta_inicio == horario_volta_fim:
+                                msg  += f"VOLTA: {horario_volta_inicio}\n"
+                            else:
+                                msg  += f"VOLTA: {horario_volta_inicio}-{horario_volta_fim}\n"
+                    else:
+                        msg = "Nenhum horário cadastrado no momento"
                 except: 
                     msg = "Nenhum horário cadastrado no momento"
             update.message.reply_text(msg)
@@ -124,20 +158,29 @@ class Caroneiro(object):
     def remove_horario(self, update, context):
         channel = update.message.chat.type
         if channel == 'private':
-            chat_id = str(update.message.chat.id)
-            idx = np.where(self.horarios[:,0]==chat_id)[0][0]
-            self.horarios = np.delete(self.horarios,idx,0)
-    
+            args = context.args
+            if len(args) > 0:
+                trajeto = args[0]
+                chat_id = str(update.message.chat.id)
+                idx = np.where(self.horarios[:,0]==chat_id)[0][0]
+                if trajeto == "ida":
+                    self.horarios[idx,1] = "0:00"
+                    self.horarios[idx,2] = "0:00"
+                    msg = "Horário de IDA removido."
+                if trajeto == "volta":
+                    self.horarios[idx,3] = "0:00"
+                    self.horarios[idx,4] = "0:00"        
+                    msg = "Horário de VOLTA removido."
+            update.message.reply_text(msg)
+
     def avisa(self, update, context):
         channel = update.message.chat.type
-        print('foo1')
         if channel == 'private':
             self.ouvir = True
             update.message.reply_text("Aviso LIGADO")
 
     def silencia(self, update, context):
         channel = update.message.chat.type
-        print('foo2')
         if channel == 'private':
             self.ouvir = False
             update.message.reply_text("Aviso DESLIGADO.")
